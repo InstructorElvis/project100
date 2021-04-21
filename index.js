fc8ccd472a9ff4c07630faaf8c3380a103729ee8
Elvis Garcia <Elvis@Elvis-MacBook-Air.local>
2021-04-21 12:06:28 -0400

    Include an h1 and p tag on index.html

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ console.log("Hello World");
 def hello():
     print("Hello")
 
+document.querySelector("p").addEventListener("click", () => {
+    document.querySelector("h1").innerHTML = "Elvis is Even Greater than Before! AND I am inside of the 'testBranch' Branch on Git";
+    
+});
+
     
